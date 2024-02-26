6516fd5ec3cb1c5f68836079c8e7526d23afeeb3
Marcus Barros <marcus.mvbs@gmail.com>
2024-02-26 16:54:14 -0500

    Argocd integration test v4.0

--- kind_build.ps1
+++ kind_build.ps1
@@ -5,6 +5,7 @@ $network_type   = "--network=host"
 $socket_volume  = "/var/run/docker.sock:/var/run/docker.sock"
 $playbook_exec  = "ansible-playbook -i Ansible/inventory.ini Ansible/playbook.yaml"
 $argocd_install = "kubectl apply -n argocd-ns -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+$kubectl_pods   = "kubectl get pods -A"
 $apply_app      = "kubectl apply -f application.yaml"
 $kyverno_config = "kubectl apply -f Kind/charts/dev/kyverno/templates/clusterpolicy.yaml"
 $nginx_config   = "kubectl apply -f Kind/charts/dev/nginx/templates/deployment.yaml"
@@ -21,6 +22,7 @@ $Apply_ArgoCD  = "docker exec -it $containerName sh -c '$argocd_install'"
 $Apply_ArgoApp = "docker exec -it $containerName sh -c '$apply_app'"
 
 # Kubernetes Environment Variables
+$KubectlGetPods = "docker exec -it $containerName sh -c '$kubectl_pods'"
 $Apply_Kyverno = "docker exec -it $containerName sh -c '$kyverno_config'"
 $Apply_Nginx   = "docker exec -it $containerName sh -c '$nginx_config'"
 
@@ -38,10 +40,13 @@ Start-Sleep -Seconds 10
 # Argocd install and manifest application ##
 Invoke-Expression -Command $Apply_ArgoCD
 Invoke-Expression -Command $Apply_ArgoApp
-
+Start-Sleep -Seconds 15
+Invoke-Expression -Command $KubectlGetPods
 # # Apply Kubernetes config
 # Start-Sleep -Seconds 120
 # Invoke-Expression -Command $Apply_Kyverno
 
 # # Create nginx pod using Kyverno policy of cpu and memory defined
-# Invoke-Expression -Command $Apply_Nginx
+Invoke-Expression -Command $Apply_Nginx
+Start-Sleep -Seconds 5
+Invoke-Expression -Command $KubectlGetPods

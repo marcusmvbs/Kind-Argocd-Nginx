9e1d3dc01fe9c98c061c4d8e70ebb56fd4eba2b6
Marcus Barros <marcus.mvbs@gmail.com>
2024-02-26 12:10:02 -0500

    Argocd integration test v1.1

---
+++ kind_remove.ps1
@@ -0,0 +1,20 @@
+# Common Variables
+$containerName  = "kind_container"
+
+# Docker Variables
+$KindDelCmd      = "docker exec -it $containerName sh -c 'kind delete cluster'"
+$DockerStopCmd   = "docker stop $containerName"
+$DockerRemoveCmd = "docker rm $containerName"
+$DockerImagePrune = "docker image prune --all --force"
+$DockerSystemPrune = "docker system prune -f"
+
+# Execute Docker container to delete kind cluster
+Invoke-Expression -Command $KindDelCmd
+# Stop the Docker container
+Invoke-Expression -Command $DockerStopCmd
+# Remove the Docker container
+Invoke-Expression -Command $DockerRemoveCmd
+# Remove Docker images
+Invoke-Expression -Command $DockerImagePrune
+# Remove Docker volumes
+Invoke-Expression -Command $DockerSystemPrune

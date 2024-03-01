d588d41f5608f2ac9402514b5659ce6aa8de68b7
Marcus Barros <marcus.mvbs@gmail.com>
2024-03-01 17:50:32 -0500

    Argocd synced v1.3

--- 2.0argocd_config.ps1
+++ 2.0argocd_config.ps1
@@ -4,4 +4,4 @@ $Argocd_Script = "docker exec -it $containerName sh -c 'kind/argocd_config.sh'"
 
 Invoke-Expression -Command $Argocd_Script
 
-Write-Output "---`nArgoCD is configured. Apps created and synced.`n"
+Write-Output "---`nArgoCD is configured. Nginx app created and synced.`n"
--- Kind/argocd_config.sh
+++ Kind/argocd_config.sh
@@ -28,7 +28,9 @@ GITHUB_REPO="https://github.com/marcusmvbs/argocd-features.git"
 argocd login $ARGOCD_SERVER --username admin --password $init_argo_pswd_output --skip-test-tls --insecure
 sleep 5
 argocd cluster add kind-kind --server=$ARGOCD_SERVER --insecure -y
-sleep 5
+sleep 10
 argocd app create nginx-webapp --repo $GITHUB_REPO --path charts/dev/nginx --values values.yaml --dest-namespace webserver --dest-server $endpoint_k
-sleep 5
+sleep 10
 argocd app sync nginx-webapp
+sleep 10
+argocd app get nginx-webapp

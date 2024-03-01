ddeb9a35689f8b20da5117cde2b50605ec39ed78
Marcus Barros <marcus.mvbs@gmail.com>
2024-03-01 17:57:35 -0500

    Nginx sync test v1.0

--- 2.0argocd_config.ps1
+++ 2.0argocd_config.ps1
@@ -1,7 +1,10 @@
 # Argocd configuration
 $containerName = "kind_container"
+$nginx_pods    = "kubectl get pods -n webserver"
+
 $Argocd_Script = "docker exec -it $containerName sh -c 'kind/argocd_config.sh'"
+$Nginx_Pods    = "docker exec -it $containerName sh -c '$nginx_pods'"
 
 Invoke-Expression -Command $Argocd_Script
-
+Invoke-Expression -Command $Nginx_Pods
 Write-Output "---`nArgoCD is configured. Nginx app created and synced.`n"

3db68c20f5521e520596f199d3af5c639feaf806
Marcus Barros <marcus.mvbs@gmail.com>
2024-03-01 15:24:24 -0500

    argocd config script v2.0.1

--- Kind/argocd_config.sh
+++ Kind/argocd_config.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 
 ## Argocd Configuration ##
-kubectl_argo_pods=$(kubectl get pods -n argocd)
-init_argo_pswd=$(argocd admin initial-password -n argocd | awk 'NR==1 {print $1}')
+init_argo_pswd_output=$(argocd admin initial-password -n argocd | awk 'NR==1 {print $1}')
 endpoint=$(kubectl get endpoints kubernetes -o=jsonpath='{.subsets[0].addresses[0].ip}:{.subsets[0].ports[0].port}')
 endpoint_k="https://$endpoint"
+echo "Initial ArgoCD Password: $init_argo_pswd_output"
+echo "Endpoint for Kubernetes: $endpoint_k"
 
 # Update kube config with correct endpoint
 awk -v endpoint_k8s="$endpoint_k" '/server: /{$2 = endpoint_k8s} 1' ~/.kube/config > temp && mv temp ~/.kube/config
-
 # Fix kube config indentation
 sed -i 's/\(^ *server:\)/    \1/' ~/.kube/config
-
 # Update application.yaml with correct endpoint
 sed -i "s#https://kubernetes.default.svc#$endpoint_k#" ../application.yaml
 
-# Apply configuration changes
-kubectl_argo_pods_output=$(eval "$kubectl_argo_pods")
-init_argo_pswd_output=$(eval "$init_argo_pswd")
-apply_app_output=$(eval "$apply_app")
-config_set_output=$(eval "$config_set")
-config_context_output=$(eval "$config_context")
-
-## Output ##
-echo "ArgoCD Pods:"
-echo "$kubectl_argo_pods_output"
-
-echo "Initial ArgoCD Password:"
-echo "$init_argo_pswd_output"
-
-echo "Endpoint for Kubernetes:"
-echo "$endpoint"
-
-echo "ArgoCD app creation:"
-echo "$apply_app_output"
-
-echo "Kubernetes set-cluster kind-kind:"
-echo "$config_set_output"
-
-echo "Kubernetes set current context at namespace argocd"
-echo "$config_context_output"
-
-# Uncomment to start port forwarding
-#kubectl port-forward service/argocd-server -n argocd 8080:443 &
+# # Apply configuration changes
+apply_app_output=$(kubectl apply -f application.yaml)
+config_set_output=$(kubectl config set-cluster kind-kind --server=$endpoint_k)
+config_context_output=$(kubectl config set-context --current --namespace=argocd)
+# port_forward="$(kubectl port-forward service/argocd-server -n argocd 8080:443 &)"
--- kind_build.ps1
+++ kind_build.ps1
@@ -5,6 +5,7 @@ $network_type        = "--network=host"
 $socket_volume       = "/var/run/docker.sock:/var/run/docker.sock"
 $playbook_exec       = "ansible-playbook -i ansible/inventory.ini ansible/playbook.yaml"
 $argocd_install      = "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+$kubectl_pods        = "kubectl get pods -n argocd"
 $kubectl_endpoints   = "kubectl get endpoints"
 $dos2unix_argocd     = "dos2unix kind/argocd_config.sh"
 $dos2unix_nginx_sync = "dos2unix kind/argo_nginx_sync.sh"
@@ -19,6 +20,7 @@ $AnsiblePlaybook = "docker exec -it $containerName sh -c '$playbook_exec'"
 
 # ArgoCD variables
 $Install_ArgoCD = "docker exec -it $containerName sh -c '$argocd_install'"
+$Argo_Pods      = "docker exec -it $containerName sh -c '$kubectl_pods'"
 
 # Kubernetes Environment Variables
 $K8s_Endpoints   = "docker exec -it $containerName sh -c '$kubectl_endpoints'"
@@ -41,6 +43,7 @@ Start-Sleep -Seconds 10
 Invoke-Expression -Command $Install_ArgoCD
 Write-Output "Waiting for argocd pods creation..."
 Start-Sleep -Seconds 80
+Invoke-Expression -Command $Argo_Pods
 
 Write-Output "Cluster kubernetes is ready for argocd configuration!"
 Invoke-Expression -Command $Bad_Interp_Fix1

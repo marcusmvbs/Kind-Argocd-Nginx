7033d970a4405ec95f12bb9c2d2b2197fb5ee67b
Marcus Barros <marcus.mvbs@gmail.com>
2024-03-01 16:58:36 -0500

    argocd script completed v1.0

--- Kind/argo_nginx_sync.sh
+++ Kind/argo_nginx_sync.sh
@@ -2,11 +2,11 @@
 ARGOCD_SERVER="localhost:8080"
 GITHUB_REPO="https://github.com/marcusmvbs/argocd-features.git"
 
-echo $endpoint_k
-echo $init_argo_pswd_output
 # Login to ArgoCD server
-argocd login $ARGOCD_SERVER --username admin --password $init_argo_pswd --insecure
+argocd login $ARGOCD_SERVER --username admin --password $init_argo_pswd_output --skip-test-tls --insecure
+sleep 5
 argocd cluster add kind-kind --server=$ARGOCD_SERVER --insecure -y
+sleep 5
 argocd app create nginx-webapp --repo $GITHUB_REPO --path charts/dev/nginx --values values.yaml --dest-namespace webserver --dest-server $endpoint_k
-sleep 10
+sleep 5
 argocd app sync nginx-webapp
--- Kind/argocd_config.sh
+++ Kind/argocd_config.sh
@@ -19,14 +19,18 @@ kubectl config set-context --current --namespace=argocd
 
 kubectl port-forward service/argocd-server -n argocd 8080:443 &
 sleep 10
+
+# ---
+
 ## Argocd App Creation ##
 ARGOCD_SERVER="localhost:8080"
 GITHUB_REPO="https://github.com/marcusmvbs/argocd-features.git"
+
 # Login to ArgoCD server
-argocd login $ARGOCD_SERVER --username admin --password $init_argo_pswd --skip-test-tls --insecure
-sleep 10
+argocd login $ARGOCD_SERVER --username admin --password $init_argo_pswd_output --skip-test-tls --insecure
+sleep 5
 argocd cluster add kind-kind --server=$ARGOCD_SERVER --insecure -y
-sleep 10
+sleep 5
 argocd app create nginx-webapp --repo $GITHUB_REPO --path charts/dev/nginx --values values.yaml --dest-namespace webserver --dest-server $endpoint_k
-sleep 10
+sleep 5
 argocd app sync nginx-webapp
